b0504eedf9c58963e079236db2fc3dd8fe293f6f
David Loughlin <dloughlin@gmail.com>
2025-02-27 22:24:10 +0000

    add-speed-insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/global.css'
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
 }
