3c27bde3cefa65e6d27d5e2401a17b02cf00abec
David Loughlin <dloughlin@gmail.com>
2025-03-01 17:01:04 +0000

    yotf

---
+++ pages/projects/audio-player.js
@@ -0,0 +1,30 @@
+import Layout from '../../components/layout'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+import ReactAudioPlayer from 'react-audio-player';
+
+
+export default function Post({ postData }) {
+    return (
+      <Layout>
+        <Head>
+        <title>Year of the Flood</title>
+        </Head>
+        <article>
+        <h1 className={utilStyles.headingXl}>Year of the Flood</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString= '2024-02-03'/>
+        </div>
+        <div >
+        <ReactAudioPlayer
+          src="../../public/audio/year-of-the-flodd.m4a"
+          autoPlay
+          controls
+        />
+        </div>
+          <p>Created using <a href ="https://www.ableton.com/en/note/">Ableton Note</a> and a guitar.</p> 
+      </article>
+      </Layout>
+    )
+  }
